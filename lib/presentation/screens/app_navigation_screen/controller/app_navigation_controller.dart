b6a217ce0a8af27cdd566a160d8baa2d86f4c504
Fatma Ragab <104873184+ffatmaragab@users.noreply.github.com>
2023-03-18 01:39:01 +0200

    Update app_navigation_controller.dart

--- lib/presentation/screens/app_navigation_screen/controller/app_navigation_controller.dart
+++ lib/presentation/screens/app_navigation_screen/controller/app_navigation_controller.dart
@@ -1,5 +1,5 @@
-import 'package:fatma_ragab_s_application1/core/app_export.dart';
-import 'package:fatma_ragab_s_application1/presentation/app_navigation_screen/models/app_navigation_model.dart';
+import 'package:LEKTRA/core/app_export.dart';
+import 'package:LEKTRA/presentation/app_navigation_screen/models/app_navigation_model.dart';
 
 class AppNavigationController extends GetxController {
   Rx<AppNavigationModel> appNavigationModelObj = AppNavigationModel().obs;
