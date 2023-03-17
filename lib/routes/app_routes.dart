62dd72b0da58f5b1e1a6ebe84f21b1fe8af0b4f0
Fatma Ragab <104873184+ffatmaragab@users.noreply.github.com>
2023-03-17 23:13:01 +0200

    Add files via upload

---
+++ lib/routes/app_routes.dart
@@ -0,0 +1,106 @@
+import 'package:fatma_ragab_s_application1/presentation/your_experience_container_screen/your_experience_container_screen.dart';
+import 'package:fatma_ragab_s_application1/presentation/your_experience_container_screen/binding/your_experience_container_binding.dart';
+import 'package:fatma_ragab_s_application1/presentation/add_your_experience_screen/add_your_experience_screen.dart';
+import 'package:fatma_ragab_s_application1/presentation/add_your_experience_screen/binding/add_your_experience_binding.dart';
+import 'package:fatma_ragab_s_application1/presentation/post_your_experience_screen/post_your_experience_screen.dart';
+import 'package:fatma_ragab_s_application1/presentation/post_your_experience_screen/binding/post_your_experience_binding.dart';
+import 'package:fatma_ragab_s_application1/presentation/comments_two_screen/comments_two_screen.dart';
+import 'package:fatma_ragab_s_application1/presentation/comments_two_screen/binding/comments_two_binding.dart';
+import 'package:fatma_ragab_s_application1/presentation/comments_screen/comments_screen.dart';
+import 'package:fatma_ragab_s_application1/presentation/comments_screen/binding/comments_binding.dart';
+import 'package:fatma_ragab_s_application1/presentation/comments_one_screen/comments_one_screen.dart';
+import 'package:fatma_ragab_s_application1/presentation/comments_one_screen/binding/comments_one_binding.dart';
+import 'package:fatma_ragab_s_application1/presentation/parent_profile_screen/parent_profile_screen.dart';
+import 'package:fatma_ragab_s_application1/presentation/parent_profile_screen/binding/parent_profile_binding.dart';
+import 'package:fatma_ragab_s_application1/presentation/app_navigation_screen/app_navigation_screen.dart';
+import 'package:fatma_ragab_s_application1/presentation/app_navigation_screen/binding/app_navigation_binding.dart';
+import 'package:get/get.dart';
+
+class AppRoutes {
+  static const String yourExperiencePage = '/your_experience_page';
+
+  static const String yourExperienceContainerScreen =
+      '/your_experience_container_screen';
+
+  static const String addYourExperienceScreen = '/add_your_experience_screen';
+
+  static const String postYourExperienceScreen = '/post_your_experience_screen';
+
+  static const String commentsTwoScreen = '/comments_two_screen';
+
+  static const String commentsScreen = '/comments_screen';
+
+  static const String commentsOneScreen = '/comments_one_screen';
+
+  static const String parentProfileScreen = '/parent_profile_screen';
+
+  static const String appNavigationScreen = '/app_navigation_screen';
+
+  static String initialRoute = '/initialRoute';
+
+  static List<GetPage> pages = [
+    GetPage(
+      name: yourExperienceContainerScreen,
+      page: () => YourExperienceContainerScreen(),
+      bindings: [
+        YourExperienceContainerBinding(),
+      ],
+    ),
+    GetPage(
+      name: addYourExperienceScreen,
+      page: () => AddYourExperienceScreen(),
+      bindings: [
+        AddYourExperienceBinding(),
+      ],
+    ),
+    GetPage(
+      name: postYourExperienceScreen,
+      page: () => PostYourExperienceScreen(),
+      bindings: [
+        PostYourExperienceBinding(),
+      ],
+    ),
+    GetPage(
+      name: commentsTwoScreen,
+      page: () => CommentsTwoScreen(),
+      bindings: [
+        CommentsTwoBinding(),
+      ],
+    ),
+    GetPage(
+      name: commentsScreen,
+      page: () => CommentsScreen(),
+      bindings: [
+        CommentsBinding(),
+      ],
+    ),
+    GetPage(
+      name: commentsOneScreen,
+      page: () => CommentsOneScreen(),
+      bindings: [
+        CommentsOneBinding(),
+      ],
+    ),
+    GetPage(
+      name: parentProfileScreen,
+      page: () => ParentProfileScreen(),
+      bindings: [
+        ParentProfileBinding(),
+      ],
+    ),
+    GetPage(
+      name: appNavigationScreen,
+      page: () => AppNavigationScreen(),
+      bindings: [
+        AppNavigationBinding(),
+      ],
+    ),
+    GetPage(
+      name: initialRoute,
+      page: () => YourExperienceContainerScreen(),
+      bindings: [
+        YourExperienceContainerBinding(),
+      ],
+    )
+  ];
+}
