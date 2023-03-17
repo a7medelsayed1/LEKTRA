31e28f16ed996dccf02857596dcb333edb152b1d
Fatma Ragab <104873184+ffatmaragab@users.noreply.github.com>
2023-03-18 01:52:54 +0200

    Update your_experience_container_controller.dart

--- lib/presentation/screens/your_experience_container_screen/controller/your_experience_container_controller.dart
+++ lib/presentation/screens/your_experience_container_screen/controller/your_experience_container_controller.dart
@@ -1,6 +1,6 @@
-import 'package:fatma_ragab_s_application1/core/app_export.dart';
-import 'package:fatma_ragab_s_application1/presentation/your_experience_container_screen/models/your_experience_container_model.dart';
-import 'package:fatma_ragab_s_application1/widgets/custom_bottom_bar.dart';
+import 'package:LEKTRA/core/app_export.dart';
+import 'package:LEKTRA/presentation/your_experience_container_screen/models/your_experience_container_model.dart';
+import 'package:LEKTRA/widgets/custom_bottom_bar.dart';
 
 class YourExperienceContainerController extends GetxController {
   Rx<YourExperienceContainerModel> yourExperienceContainerModelObj =
