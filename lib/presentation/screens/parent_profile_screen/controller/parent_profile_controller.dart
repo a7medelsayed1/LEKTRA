043c65abdfd475a28c5b66857698cca00f9a2492
Fatma Ragab <104873184+ffatmaragab@users.noreply.github.com>
2023-03-18 01:49:08 +0200

    Update parent_profile_controller.dart

--- lib/presentation/screens/parent_profile_screen/controller/parent_profile_controller.dart
+++ lib/presentation/screens/parent_profile_screen/controller/parent_profile_controller.dart
@@ -1,6 +1,6 @@
-import 'package:fatma_ragab_s_application1/core/app_export.dart';
-import 'package:fatma_ragab_s_application1/presentation/parent_profile_screen/models/parent_profile_model.dart';
-import 'package:fatma_ragab_s_application1/widgets/custom_bottom_bar.dart';
+import 'package:LEKTRA/core/app_export.dart';
+import 'package:LEKTRA/presentation/parent_profile_screen/models/parent_profile_model.dart';
+import 'package:LEKTRA/widgets/custom_bottom_bar.dart';
 
 class ParentProfileController extends GetxController {
   Rx<ParentProfileModel> parentProfileModelObj = ParentProfileModel().obs;
