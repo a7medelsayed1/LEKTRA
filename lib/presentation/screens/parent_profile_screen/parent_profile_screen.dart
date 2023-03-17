4ba01b8754cb77afdda656e5272da1efac6ad09c
Fatma Ragab <104873184+ffatmaragab@users.noreply.github.com>
2023-03-18 01:48:39 +0200

    Update parent_profile_screen.dart

--- lib/presentation/screens/parent_profile_screen/parent_profile_screen.dart
+++ lib/presentation/screens/parent_profile_screen/parent_profile_screen.dart
@@ -1,10 +1,10 @@
 import 'controller/parent_profile_controller.dart';
-import 'package:fatma_ragab_s_application1/core/app_export.dart';
-import 'package:fatma_ragab_s_application1/presentation/your_experience_page/your_experience_page.dart';
-import 'package:fatma_ragab_s_application1/widgets/app_bar/appbar_image.dart';
-import 'package:fatma_ragab_s_application1/widgets/app_bar/custom_app_bar.dart';
-import 'package:fatma_ragab_s_application1/widgets/custom_bottom_bar.dart';
-import 'package:fatma_ragab_s_application1/widgets/custom_button.dart';
+import 'package:LEKTRA/core/app_export.dart';
+import 'package:LEKTRA/presentation/your_experience_page/your_experience_page.dart';
+import 'package:LEKTRA/widgets/app_bar/appbar_image.dart';
+import 'package:LEKTRA/widgets/app_bar/custom_app_bar.dart';
+import 'package:LEKTRA/widgets/custom_bottom_bar.dart';
+import 'package:LEKTRA/widgets/custom_button.dart';
 import 'package:flutter/material.dart';
 
 class ParentProfileScreen extends GetWidget<ParentProfileController> {
