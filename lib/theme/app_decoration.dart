533530ca8b70cfec1d26801239b4fdb78eea1398
Fatma Ragab <104873184+ffatmaragab@users.noreply.github.com>
2023-03-17 23:14:27 +0200

    Add files via upload

---
+++ lib/theme/app_decoration.dart
@@ -0,0 +1,112 @@
+import 'package:flutter/material.dart';
+import 'package:fatma_ragab_s_application1/core/app_export.dart';
+
+class AppDecoration {
+  static BoxDecoration get outlineGray60001 => BoxDecoration(
+        color: ColorConstant.gray50002,
+        border: Border.all(
+          color: ColorConstant.gray60001,
+          width: getHorizontalSize(
+            1,
+          ),
+        ),
+      );
+  static BoxDecoration get fillGray50002 => BoxDecoration(
+        color: ColorConstant.gray50002,
+      );
+  static BoxDecoration get outlineGray400 => BoxDecoration(
+        border: Border.all(
+          color: ColorConstant.gray400,
+          width: getHorizontalSize(
+            1,
+          ),
+        ),
+      );
+  static BoxDecoration get outlineGray50001 => BoxDecoration(
+        border: Border.all(
+          color: ColorConstant.gray50001,
+          width: getHorizontalSize(
+            1,
+          ),
+        ),
+      );
+  static BoxDecoration get fillGray300 => BoxDecoration(
+        color: ColorConstant.gray300,
+      );
+  static BoxDecoration get outlineGray8001 => BoxDecoration(
+        color: ColorConstant.gray300,
+        border: Border(
+          top: BorderSide(
+            color: ColorConstant.gray800,
+            width: getHorizontalSize(
+              1,
+            ),
+          ),
+        ),
+      );
+  static BoxDecoration get fillGray200 => BoxDecoration(
+        color: ColorConstant.gray200,
+      );
+  static BoxDecoration get fillGray20001 => BoxDecoration(
+        color: ColorConstant.gray20001,
+      );
+  static BoxDecoration get fillWhiteA700 => BoxDecoration(
+        color: ColorConstant.whiteA700,
+      );
+  static BoxDecoration get outlineGray800 => BoxDecoration(
+        color: ColorConstant.gray300,
+        border: Border(
+          bottom: BorderSide(
+            color: ColorConstant.gray800,
+            width: getHorizontalSize(
+              1,
+            ),
+          ),
+        ),
+      );
+}
+
+class BorderRadiusStyle {
+  static BorderRadius roundedBorder15 = BorderRadius.circular(
+    getHorizontalSize(
+      15,
+    ),
+  );
+
+  static BorderRadius roundedBorder10 = BorderRadius.circular(
+    getHorizontalSize(
+      10,
+    ),
+  );
+
+  static BorderRadius circleBorder21 = BorderRadius.circular(
+    getHorizontalSize(
+      21,
+    ),
+  );
+
+  static BorderRadius customBorderBL50 = BorderRadius.only(
+    bottomLeft: Radius.circular(
+      getHorizontalSize(
+        50,
+      ),
+    ),
+    bottomRight: Radius.circular(
+      getHorizontalSize(
+        50,
+      ),
+    ),
+  );
+
+  static BorderRadius circleBorder61 = BorderRadius.circular(
+    getHorizontalSize(
+      61,
+    ),
+  );
+
+  static BorderRadius circleBorder18 = BorderRadius.circular(
+    getHorizontalSize(
+      18,
+    ),
+  );
+}
---
+++ lib/theme/app_style.dart
@@ -0,0 +1,211 @@
+import 'package:flutter/material.dart';
+import 'package:fatma_ragab_s_application1/core/app_export.dart';
+
+class AppStyle {
+  static TextStyle txtInterMedium18 = TextStyle(
+    color: ColorConstant.gray500,
+    fontSize: getFontSize(
+      18,
+    ),
+    fontFamily: 'Inter',
+    fontWeight: FontWeight.w500,
+  );
+
+  static TextStyle txtInterRegular12Black9001 = TextStyle(
+    color: ColorConstant.black900,
+    fontSize: getFontSize(
+      12,
+    ),
+    fontFamily: 'Inter',
+    fontWeight: FontWeight.w400,
+  );
+
+  static TextStyle txtInterSemiBold16Black900 = TextStyle(
+    color: ColorConstant.black900,
+    fontSize: getFontSize(
+      16,
+    ),
+    fontFamily: 'Inter',
+    fontWeight: FontWeight.w600,
+  );
+
+  static TextStyle txtInterSemiBold12Gray70001 = TextStyle(
+    color: ColorConstant.gray70001,
+    fontSize: getFontSize(
+      12,
+    ),
+    fontFamily: 'Inter',
+    fontWeight: FontWeight.w600,
+  );
+
+  static TextStyle txtInterBold13Gray900 = TextStyle(
+    color: ColorConstant.gray900,
+    fontSize: getFontSize(
+      13,
+    ),
+    fontFamily: 'Inter',
+    fontWeight: FontWeight.w700,
+  );
+
+  static TextStyle txtInterMedium16Indigo600dd = TextStyle(
+    color: ColorConstant.indigo600Dd,
+    fontSize: getFontSize(
+      16,
+    ),
+    fontFamily: 'Inter',
+    fontWeight: FontWeight.w500,
+  );
+
+  static TextStyle txtInterRegular10 = TextStyle(
+    color: ColorConstant.black900,
+    fontSize: getFontSize(
+      10,
+    ),
+    fontFamily: 'Inter',
+    fontWeight: FontWeight.w400,
+  );
+
+  static TextStyle txtInterMedium13 = TextStyle(
+    color: ColorConstant.gray900,
+    fontSize: getFontSize(
+      13,
+    ),
+    fontFamily: 'Inter',
+    fontWeight: FontWeight.w500,
+  );
+
+  static TextStyle txtInterSemiBold12 = TextStyle(
+    color: ColorConstant.gray500,
+    fontSize: getFontSize(
+      12,
+    ),
+    fontFamily: 'Inter',
+    fontWeight: FontWeight.w600,
+  );
+
+  static TextStyle txtInterMedium16 = TextStyle(
+    color: ColorConstant.indigo600,
+    fontSize: getFontSize(
+      16,
+    ),
+    fontFamily: 'Inter',
+    fontWeight: FontWeight.w500,
+  );
+
+  static TextStyle txtInterBold12 = TextStyle(
+    color: ColorConstant.blueGray400,
+    fontSize: getFontSize(
+      12,
+    ),
+    fontFamily: 'Inter',
+    fontWeight: FontWeight.w700,
+  );
+
+  static TextStyle txtInterMedium14 = TextStyle(
+    color: ColorConstant.whiteA700,
+    fontSize: getFontSize(
+      14,
+    ),
+    fontFamily: 'Inter',
+    fontWeight: FontWeight.w500,
+  );
+
+  static TextStyle txtInterSemiBold16 = TextStyle(
+    color: ColorConstant.gray900,
+    fontSize: getFontSize(
+      16,
+    ),
+    fontFamily: 'Inter',
+    fontWeight: FontWeight.w600,
+  );
+
+  static TextStyle txtInterRegular12Black900 = TextStyle(
+    color: ColorConstant.black900,
+    fontSize: getFontSize(
+      12,
+    ),
+    fontFamily: 'Inter',
+    fontWeight: FontWeight.w400,
+  );
+
+  static TextStyle txtInterBold13 = TextStyle(
+    color: ColorConstant.indigo600,
+    fontSize: getFontSize(
+      13,
+    ),
+    fontFamily: 'Inter',
+    fontWeight: FontWeight.w700,
+  );
+
+  static TextStyle txtInterMedium8 = TextStyle(
+    color: ColorConstant.gray70002,
+    fontSize: getFontSize(
+      8,
+    ),
+    fontFamily: 'Inter',
+    fontWeight: FontWeight.w500,
+  );
+
+  static TextStyle txtInterSemiBold12Black900 = TextStyle(
+    color: ColorConstant.black900,
+    fontSize: getFontSize(
+      12,
+    ),
+    fontFamily: 'Inter',
+    fontWeight: FontWeight.w600,
+  );
+
+  static TextStyle txtInterSemiBold10 = TextStyle(
+    color: ColorConstant.black900,
+    fontSize: getFontSize(
+      10,
+    ),
+    fontFamily: 'Inter',
+    fontWeight: FontWeight.w600,
+  );
+
+  static TextStyle txtRobotoRegular16 = TextStyle(
+    color: ColorConstant.bluegray400,
+    fontSize: getFontSize(
+      16,
+    ),
+    fontFamily: 'Roboto',
+    fontWeight: FontWeight.w400,
+  );
+
+  static TextStyle txtInterMedium14Gray50001 = TextStyle(
+    color: ColorConstant.gray50001,
+    fontSize: getFontSize(
+      14,
+    ),
+    fontFamily: 'Inter',
+    fontWeight: FontWeight.w500,
+  );
+
+  static TextStyle txtInterRegular12 = TextStyle(
+    color: ColorConstant.gray700,
+    fontSize: getFontSize(
+      12,
+    ),
+    fontFamily: 'Inter',
+    fontWeight: FontWeight.w400,
+  );
+
+  static TextStyle txtInterBold12WhiteA700 = TextStyle(
+    color: ColorConstant.whiteA700,
+    fontSize: getFontSize(
+      12,
+    ),
+    fontFamily: 'Inter',
+    fontWeight: FontWeight.w700,
+  );
+
+  static TextStyle txtRobotoRegular20 = TextStyle(
+    color: ColorConstant.black900,
+    fontSize: getFontSize(
+      20,
+    ),
+    fontFamily: 'Roboto',
+    fontWeight: FontWeight.w400,
+  );
+}
